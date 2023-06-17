0613992f2c06d48f477213a60c89556b54613161
woiswzx <5889210@qq.com>
2023-06-18 00:27:22 +0800

    Initial commit

---
+++ java-foundational/src/main/java/net/woiswzx/java/Helloword.java
@@ -0,0 +1,13 @@
+package net.woiswzx.java;/**
+ * 我的第一个Java程序
+ *
+ * @author woiswzx 5889210@qq.com
+ * @Since JDK1.8
+ * @version - 10:25
+ */
+
+public class Helloword {
+    public static void main(String[] args){
+        System.out.println("hello world with windows11 & JDK1.8");
+    }
+}
---
+++ java-foundational/src/main/java/net/woiswzx/java/Main.java
@@ -0,0 +1,11 @@
+package net.woiswzx.java;
+
+/**
+ * @author Wzhx
+ * @version ${DATA} - ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
