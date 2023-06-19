9081d670bfbee0d9447758ed3cd6c08171f9c2c2
woiswzx <5889210@qq.com>
2023-06-19 10:57:55 +0800

    我的第一个Java程序

---
+++ java-foundational/src/main/java/net/woiswzx/java/foundational/object/HelloWorld.java
@@ -0,0 +1,15 @@
+package net.woiswzx.java.foundational.object;
+
+/**
+ * 我的第一个Java程序
+ *
+ * @author woiswzx 5889210@qq.com
+ * @version 2023.6.19 - 10:52
+ * @Since JDK1.8
+ */
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello world with windows11 & jdk1.8");
+    }
+}
